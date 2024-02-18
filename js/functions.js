c1957413c77837a2ab2282aa44d4c8532ea5ef0a
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-02-18 23:16:17 +0500

    начала делать вторую функцию

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+let counterStringLength = (string, maxLength) => {
+  if (string.lenght() <= maxLength) {
+    return true;
+  }
+
+  return false;
+}
+
+let palindromeCheck = (stringCheck) => {
+  let string = stringCheck.replaceAll();
+  let UpperCaseString = string.toUpperCase();
+  let emptyLine = '';
+
+  for (let i = 1;;) {
+  }
+}
