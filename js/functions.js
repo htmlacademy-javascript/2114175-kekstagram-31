21ec70653dcfda3ce9b6ce83c1f507cdefcc7f00
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-02-19 23:32:17 +0500

    выполнена третья функция

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
 // Функция для проверки длины строки с максимально допустимой длиной
-let counterStringLength = (string, maxLength) => {
+const counterStringLength = (string, maxLength) => {
   if (string.lenght() <= maxLength) {
     return true;
   }
 
   return false;
-}
+};
 
 // Функция для проверки, является ли строка палиндромом
-let palindromeCheck = (string) => {
-  let stringReplace = string.replaceAll(' ', '');
-  let normalizeString = stringReplace.toUpperCase();
+const palindromeCheck = (string) => {
+  const stringReplace = string.replaceAll(' ', '');
+  const normalizeString = stringReplace.toUpperCase();
   let reverseString = '';
 
   for (let i = normalizeString.length - 1; i >= 0; i--) {
@@ -18,18 +18,18 @@ let palindromeCheck = (string) => {
   }
 
   return normalizeString === reverseString;
-}
+};
 
 // Функция принимает строку, извлекает цифры, возвращает их числом или NaN
-let stringToNumber = (string) => {
+const stringToNumber = (string) => {
   string = string.toString();
   let numbers = '';
   for (let i = 0; i <= string.length - 1; i++) {
-    let char = string.at(i);
-    let charInt = parseInt(char, 10);
+    const char = string.at(i);
+    const charInt = parseInt(char, 10);
     if (!Number.isNaN(charInt)) {
       numbers += char;
     }
   }
   return parseInt(numbers, 10);
-}
+};
