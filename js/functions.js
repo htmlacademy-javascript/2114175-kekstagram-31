b0061fd6ce7bb06ef8b34fa7e74a7e82aca399fa
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-03-01 23:15:32 +0500

    написала тренеровочную функцию, в задание написано что этот файл должен быть подключен к индекс не как модуль

--- js/functions.js
+++ js/functions.js
@@ -30,4 +30,51 @@ const stringToNumber = (string) => {
 
 counterStringLength('123', 3);
 palindromeCheck('поп');
-stringToNumber('123');
+
+// Делу — время
+// тут использую тренировочную функцию которая писали до этого
+const timeCheck = (workStartTime, workEndTime, meetStartTime, meetDuration) => {
+  workStartTime = workStartTime.split(':');
+  workEndTime = workEndTime.split(':');
+  meetStartTime = meetStartTime.split(':');
+  const workStartHour = stringToNumber(workStartTime[0]);
+  const workStartMinute = stringToNumber(workStartTime[1]);
+  const workEndTimeHour = stringToNumber(workEndTime[0]);
+  const workEndTimeMinute = stringToNumber(workEndTime[1]);
+  const meetStartHour = stringToNumber(meetStartTime[0]);
+  const meetStartMinute = stringToNumber(meetStartTime[1]);
+  const meetDurationHour = Math.floor(meetDuration / 60);
+  const meetDurationMinute = meetDuration - (meetDurationHour * 60);
+
+  // проверка на время до раб.времени
+  if (meetStartHour < workStartHour) {
+    return false;
+  } else if (meetStartHour === workStartHour && meetStartMinute < workStartMinute) {
+    return false;
+  }
+  // проверка на окончание раб.времени
+  if (meetStartHour > workEndTimeHour) {
+    return false;
+  } else if (meetStartHour === workEndTimeHour && meetStartMinute > workEndTimeMinute) {
+    return false;
+  }
+  //проверка на окончание митинга
+  let meetEndHour = meetDurationHour + meetStartHour;
+  let meetEndMinute = meetDurationMinute + meetStartMinute;
+  // переводим митинг в часы и минуты
+  if (meetEndMinute >= 60) {
+    const hours = Math.floor(meetEndMinute / 60);
+    meetEndMinute = meetEndMinute - (hours * 60);
+    meetEndHour += hours;
+  }
+  //  проверка оканчания митинга и конца рабочего времени
+  if (meetEndHour > workEndTimeHour) {
+    return false;
+  } else if (meetEndHour === workEndTimeHour && meetEndMinute > workEndTimeMinute) {
+    return false;
+  }
+
+  return true;
+};
+
+timeCheck('08:00', '17:30', '14:00', 90);
