8cf6f48ed883e9be884b07d7b4c0d4725c5dc868
Keks <keks@htmlacademy.ru>
2024-03-07 00:20:35 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/photo-card.js
@@ -0,0 +1,35 @@
+import {getRandomArrayElement, getRandomInteger, createIdGeneration} from './util.js';
+import {createUserComment} from './comment-card.js';
+
+// данные для блока фотографии
+// id фотографии
+const getPhotoId = createIdGeneration();
+
+// description фотки
+const PHOTO_DESCRIPTIONS = [
+  'Я старался',
+  'Почти получилось хорошо',
+  'Получилось не очень',
+  'Мне очень понравилось',
+  'Нравится композиция',
+  'Было забавно'
+];
+
+// собираем блок для фотки
+const createPhotoCard = () => {
+  const id = getPhotoId();
+  const countComments = getRandomInteger(0, 30);
+  const comments = Array.from({length: countComments}, createUserComment);
+
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
+    likes: getRandomInteger(15, 200),
+    comments
+  };
+};
+
+const photoCardList = (count) => Array.from({length: count}, createPhotoCard);
+
+export {photoCardList};
