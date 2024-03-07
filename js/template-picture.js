c07b0b247cf7481e59fd197e0c697c2ecf1dde43
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-03-07 20:44:38 +0500

    домашка

--- js/template-picture.js
+++ js/template-picture.js
@@ -1,11 +1,11 @@
-import {photoCardList} from './photo-card';
+import {createPhotoCardList} from './photo-card';
 
-//шаблон и индекса
+//шаблон
 const templateElement = document.querySelector('#picture').content.querySelector('.picture');
 // контайнер куда будем загружать фотки
 const container = document.querySelector('.pictures');
 // количество отображаемых фото
-const photoMock = photoCardList(25);
+const photoMock = createPhotoCardList(25);
 // создам 'черный ящик'
 const listPicturesFragments = document.createDocumentFragment();
 
@@ -13,7 +13,6 @@ photoMock.forEach((photo) => {
   // склонированный блок из шаблона
   const photoCard = templateElement.cloneNode(true);
   photoCard.href = `${photo.id}`;
-  photoCard.dataset.id = photo.id;
   const photoCardImg = photoCard.querySelector('.picture__img');
   photoCardImg.src = photo.url;
   photoCardImg.alt = photo.description;
