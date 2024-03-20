d0b849f825a2b9c48696448c645c4ddf9d1fa749
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-03-21 02:07:13 +0500

    домашка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import {createPhotoCardList} from './photo-card.js'; // функция с фотками из папки списком
+import { createPhotoCardList } from './photo-card.js'; // функция с фотками из папки списком
 import { savePhotos } from './photo-state.js'; // фукнция записывает в массив входяящие фотки
-import {renderThumbnails} from './template-picture.js'; // функция с готовым массивом созданная из миниатюр
+import { renderThumbnails } from './template-picture.js'; // функция с готовым массивом созданная из миниатюр
+import './upload-form.js'; // подключаю на время пока что
 
 const photos = createPhotoCardList(25);// создаем список фотографий
 savePhotos(photos);// переписываем их в массив
