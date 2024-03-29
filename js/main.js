e8963ac5a2cfb912dd307094e6f4c5854e2b083c
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-03-29 21:50:33 +0500

    отработка ошибки

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import { savePhotos } from './photo-state.js'; // фукнция записывает в массив входяящие фотки
 import { renderThumbnails } from './template-picture.js'; // функция с готовым массивом созданная из миниатюр
 import './upload-form.js'; // подключаю на время пока что
+import { renderError } from './template-err.js';
 
 fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
   .then((response) => response.json())
   .then((data) => {
     savePhotos(data);// переписываем их в массив
     renderThumbnails(data); // и создаем массив для миниатюр на основе массива выше
-  });
-
+  })
+  .catch(renderError);
