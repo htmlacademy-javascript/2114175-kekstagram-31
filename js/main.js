53fb5620dc6dce8f1b0f5f2eec644f4b9f701fa0
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-03-29 21:16:02 +0500

    сделала get дата  апи

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-import { createPhotoCardList } from './photo-card.js'; // функция с фотками из папки списком
 import { savePhotos } from './photo-state.js'; // фукнция записывает в массив входяящие фотки
 import { renderThumbnails } from './template-picture.js'; // функция с готовым массивом созданная из миниатюр
 import './upload-form.js'; // подключаю на время пока что
 
-const photos = createPhotoCardList(25);// создаем список фотографий
-savePhotos(photos);// переписываем их в массив
-renderThumbnails(photos); // и создаем массив для миниатюр на основе массива выше
+fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((data) => {
+    savePhotos(data);// переписываем их в массив
+    renderThumbnails(data); // и создаем массив для миниатюр на основе массива выше
+  });
+
